a9bda933fb323c045520a890ddb8c031715a1295
gabrielbaptista <gabriel.baptista@uninove.br>
2015-11-14 10:59:20 -0200

    Alterações no banco

---
+++ Fontes/SQLDatabaseProj/SQLDatabaseProj/Tables/Projetos.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Projetos]
+(
+	[Id_Proj] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] VARCHAR(1024) NOT NULL, 
+    [Id_User] INT NOT NULL, 
+    CONSTRAINT [FK_Projetos_Usuarios] FOREIGN KEY ([Id_User]) REFERENCES [Usuarios]([Id_User])
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_Projetos_Nome] ON [dbo].[Projetos] ([Nome])
