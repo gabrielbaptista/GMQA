3da10acb6736f93ba428914ac07ff9d359b75207
Patrick Wellington Duarte de Oliveira <patrick_ilherme@hotmail.com>
2015-11-18 21:56:21 -0200

    Atualizando
    Coloquei a chave FK de cadastrar usuario, com a tabela usuarios;

--- Fontes/SQLDatabaseProj/SQLDatabaseProj/Tables/Cadastrar_Usuario.sql
+++ Fontes/SQLDatabaseProj/SQLDatabaseProj/Tables/Cadastrar_Usuario.sql
@@ -3,7 +3,8 @@
 	[Id_usuario] INT NOT NULL PRIMARY KEY IDENTITY,  
     [nome_usuario] NCHAR(10) NOT NULL, 
     [email_usuario] NCHAR(10) NOT NULL, 
-    [senha_usuario] NCHAR(10) NOT NULL
+    [senha_usuario] NCHAR(10) NOT NULL, 
+    CONSTRAINT [FK_Cadastrar_Usuario_Usuarios] FOREIGN KEY (Id_usuario) REFERENCES [Usuarios]([Id_User])
 )
 
 GO
