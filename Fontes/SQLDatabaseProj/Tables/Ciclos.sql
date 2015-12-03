554d85f1a82faf37060f3c8a1945c186eaec5960
FlavioDeAssis <f_lavio@hotmail.com>
2015-12-02 22:52:00 -0200

    Criado Tabela Etapas, seguindo DER.

---
+++ Fontes/SQLDatabaseProj/Tables/Ciclos.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[CiclosProjetos]
+(
+	[IdCiclos] INT NOT NULL PRIMARY KEY, 
+    [NumeroCiclo] INT NOT NULL, 
+    [FaseCiclo] VARCHAR(50) NOT NULL, 
+    [DataInicio] DATE NOT NULL, 
+    [DataFim] DATE NOT NULL, 
+    [IdProjeto] INT NOT NULL, 
+    [IdEtapas] INT NOT NULL, 
+    CONSTRAINT [FK_Ciclos_Projetos] FOREIGN KEY ([IdProjeto]) REFERENCES [Projetos]([IdProjeto]), 
+    CONSTRAINT [FK_Ciclos_Etapas] FOREIGN KEY ([IdEtapas]) REFERENCES [Etapas]([IdEtapas])
+)
