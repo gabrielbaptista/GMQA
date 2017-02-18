9236f28721cfd6f5c9adbcb6fa719c27f9daa51e
Rodolfo <Rodolfo@PC-RODOLFO>
2017-02-18 10:19:00 -0200

    Tabela Riscos

--- Fontes/SQLDatabaseProj/Tables/Riscos.sql
+++ Fontes/SQLDatabaseProj/Tables/Riscos.sql
@@ -2,16 +2,16 @@
 (
 	[IdRisco] INT NOT NULL PRIMARY KEY IDENTITY, 
     [DescricaoRisco] VARCHAR(50) NOT NULL, 
-    [IdProjeto] INT NOT NULL, 
-    [IdCiclo] INT NOT NULL, 
-    [Categoria] INT NOT NULL, 
-    [ProbabilidadeOcorrencia] INT NOT NULL, 
-    [EfeitoOcorrencia] INT NOT NULL, 
-    [Acao] VARCHAR(50) NOT NULL, 
-    [ResponsavelAcao] VARCHAR(50) NOT NULL, 
-    [Status] INT NOT NULL
+    [IdProjeto] INT NULL, 
+    [IdCiclo] INT NULL, 
+    [Categoria] INT NULL, 
+    [ProbabilidadeOcorrencia] INT NULL, 
+    [EfeitoOcorrencia] INT NULL, 
+    [Acao] VARCHAR(50) NULL, 
+    [ResponsavelAcao] VARCHAR(50) NULL, 
+    [Status] INT NULL
 )
 
-GO
+--GO
 
-CREATE INDEX [IX_Riscos_Column] ON [dbo].[Riscos] ([DescricaoRisco])
+----CREATE INDEX [IX_Riscos_Column] ON [dbo].[Riscos] ([DescricaoRisco])
