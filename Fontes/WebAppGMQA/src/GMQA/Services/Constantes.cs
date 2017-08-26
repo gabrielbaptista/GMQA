f34f2770c8a5ac825eb9c47e42d8e41c94c231bb
Rodolfo1997 <rodolfostopinto@hotmail.com>
2017-08-26 09:49:56 -0300

    conection string

--- Fontes/WebAppGMQA/src/GMQA/Services/Constantes.cs
+++ Fontes/WebAppGMQA/src/GMQA/Services/Constantes.cs
@@ -8,6 +8,8 @@ namespace WebAppCoreGMQA.Services
     public class Constantes
     {
         public const string CONEXAO_BANCO_LOCAL = "Server=(localdb)\\mssqllocaldb;Database=GMQA;Trusted_Connection=True;MultipleActiveResultSets=true";
-        public const string CONEXAO_BANCO = "Server=tcp:fabricaappu9.database.windows.net,1433;Initial Catalog=GMQA;Persist Security Info=False;User ID=fabrica;Password=app@2017;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        //public const string CONEXAO_BANCO = "Server=tcp:fabricaappu9.database.windows.net,1433;Initial Catalog=GMQA;Persist Security Info=False;User ID=fabrica;Password=app@2017;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public const string CONEXAO_BANCO = "Server=tcp:gmqadbserver.database.windows.net,1433;Initial Catalog=GMQA;Persist Security Info=False;User ID=gmqa;Password=Gmq@2017;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
     }
 }
